ec8fffda12ab356dd21bad1cbd2aabf29313b469
Shamila22-git <ST10488536@vcconnect.edu.za>
2025-11-10 13:57:29 +0200

    final commit

---
+++ src/Queetion1/IProductSales.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Queetion1;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public interface IProductSales {
+    int TotalSales(int[][] productSales);
+    double AverageSales(int[][] productSales);
+    int MaxSale(int[][] productSales);
+    int MinSale(int[][] productSales);
+
+}
